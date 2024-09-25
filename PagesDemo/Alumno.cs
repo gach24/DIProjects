5c440447a05812994ccaf158634d24b94f366044
GACH <germanach@educastur.org>
2024-09-25 10:42:40 +0200

    Agregar archivos de proyecto.

---
+++ PagesDemo/Alumno.cs
@@ -0,0 +1,26 @@
+﻿namespace PagesDemo
+{
+	public class Alumno
+	{
+		private string firstName;
+
+		public string FirstName
+		{
+			get
+			{
+				return firstName;
+			}
+			set
+			{
+				if (value != null) 
+					firstName = value;
+			}
+		}
+
+		public Alumno()
+		{
+			FirstName = "Juan";
+			string valor = FirstName;
+		}
+    }
+}
---
+++ PagesDemo/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace PagesDemo
+{
+	public partial class MainPage : ContentPage
+	{
+
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new ContentPageDemo());
+		}
+	}
+
+}
